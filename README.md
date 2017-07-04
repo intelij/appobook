08655ea486ecd621f3158c28546c4a71dc9c0562
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2017-07-04 20:05:06 -0300

    Remove old sass styles to fix fonts

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Appointment Book
 
 Appobook stands for appointment book. A manner of a consumer and a service provider set down an appointment without the need of phone calls or whatsApp messages
+
+
+# Up and running
+
+```
+  $ npm install
+  $ npm start
+```
--- public/src/app/app.module.js
+++ public/src/app/app.module.js
@@ -5,8 +5,9 @@ import angular from 'angular';
 import '../assets/sass/style.scss';
 
 import $scriptjs from 'scriptjs';
-// import $ from 'jquery';
-// window.jQuery = $;
+
+import $ from 'jquery';
+window.jQuery = $;
 
 /** Dependencies modules imports */
 import root from './modules/root.module';
@@ -15,11 +16,9 @@ function loadScript(bundle) {
   return new Promise(resolve => $scriptjs(bundle, resolve));
 }
 
-// loadScript([
-//   'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js'
-// ]).then(startApp);
-
-startApp();
+loadScript([
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js'
+]).then(startApp);
 
 function startApp() {
   const app = angular.module('appobook', [
